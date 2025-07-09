51a0e52811ed47bfffb53efc89b1c1ced434dd67
MrAlegria-6 <mralegria@gmail.com>
2025-07-08 20:15:07 -0600

    Se agrego Cors

--- ionspark/settings.py
+++ ionspark/settings.py
@@ -133,3 +133,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
